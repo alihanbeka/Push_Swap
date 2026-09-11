b32e8635c7d2ee2195fb2220d51aa06b04358895
Emir <emiraskkk@outlook.com>
2026-09-11 23:41:19 +0300

    functions edit

---
+++ push.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: masik <masik@student.42istanbul.com.tr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/11 23:32:21 by masik             #+#    #+#             */
+/*   Updated: 2026/09/11 23:32:23 by masik            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Push_Swap.h"
+
+void	pa(t_stack **a, t_stack **b)
+{
+	t_stack	*tmp;
+
+	if (!b || !(*b))
+		return ;
+	tmp = (*b);
+	(*b) = (*b)->next;
+	tmp->next = (*a);
+	(*a) = tmp;
+	write(1, "pa\n", 3);
+}
+
+void	pb(t_stack **a, t_stack **b)
+{
+	t_stack	*tmp;
+
+	if (!a || (!*a))
+		return ;
+	tmp = (*a);
+	(*a) = (*a)->next;
+	tmp->next = (*b);
+	(*b) = tmp;
+	write(1, "pb\n", 3);
+}
