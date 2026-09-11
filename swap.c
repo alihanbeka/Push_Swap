e27f10f362e00adea50a72f054e26b0c0f9b3953
Emir <emiraskkk@outlook.com>
2026-09-11 23:44:29 +0300

    swap.c

--- swap.c
+++ swap.c
@@ -6,39 +6,44 @@
 /*   By: masik <masik@student.42istanbul.com.tr>   #+#  +:+       +#+         */
 /*                                               +#+#+#+#+#+   +#+            */
 /*   Created: 2026/09/10 17:00:57 by masik            #+#    #+#              */
-/*   Updated: 2026/09/11 23:32:51 by masik           ###   ########.fr        */
+/*   Updated: 2026/09/11 23:44:07 by masik           ###   ########.fr        */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Push_Swap.h"
 
-void	swap(t_stack **stack)
+static int	swap(t_stack **stack)
 {
 	t_stack	*tmp;
 
 	if (!stack || !(*stack) || (*stack)->next == NULL)
-		return ;
+		return (0);
 	tmp = (*stack);
 	(*stack) = (*stack)->next;
 	tmp->next = (*stack)->next;
 	(*stack)->next = tmp;
+	return (1);
 }
 
 void	sa(t_stack **a)
 {
-	swap(a);
-	write(1, "sa\n", 3);
+	if (swap(a))
+		write(1, "sa\n", 3);
 }
 
 void	sb(t_stack **b)
 {
-	swap(b);
-	write(1, "sb\n", 3);
+	if (swap(b))
+		write(1, "sb\n", 3);
 }
 
 void	ss(t_stack **a, t_stack **b)
 {
-	swap(a);
-	swap(b);
-	write(1, "ss\n", 3);
+	int	s_a;
+	int	s_b;
+
+	s_a = swap(a);
+	s_b = swap(b);
+	if (s_a || s_b)
+		write(1, "ss\n", 3);
 }
