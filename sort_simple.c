98a86881bee345e5841095fc4741556009620744
Emir <emiraskkk@outlook.com>
2026-09-17 00:42:15 +0300

    simple sort and issorted  added

--- sort_simple.c
+++ sort_simple.c
@@ -6,7 +6,75 @@
 /*   By: masik <masik@student.42istanbul.com.tr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/15 20:45:40 by masik             #+#    #+#             */
-/*   Updated: 2026/09/16 00:59:11 by masik            ###   ########.fr       */
+/*   Updated: 2026/09/17 00:41:07 by masik            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Push_Swap.h"
+
+void	sort_simple(t_stack **a, t_stack **b)
+{
+	int		min_index;
+	t_stack	*tmp;
+
+	tmp = (*a);
+	min_index = (*a)->index;
+	while (tmp)
+	{
+		if (min_index > tmp->index)
+			min_index = tmp->index;
+		tmp = tmp->next;
+	}
+	while (tmp)
+	{
+		if (min_index != tmp->index)
+			ra(a);
+		tmp = tmp->next;
+	}
+	pb(a, b);
+}
+
+static int	get_target_position(t_stack *a, int target)
+{
+	int	pos;
+
+	pos = 0;
+	while (a != NULL)
+	{
+		if (a->index == target)
+			return (pos);
+		pos++;
+		a = a->next;
+	}
+	return (pos);
+}
+
+void	sort_simple(t_stack **a, t_stack **b)
+{
+	int	target;
+	int	pos;
+	int	size;
+
+	if (!a || !*a || !b || is_sorted(*a))
+		return ;
+	target = 0;
+	while (*a != NULL)
+	{
+		size = stack_size(*a);
+		pos = get_target_position(*a, target);
+		if (pos <= size / 2)
+		{
+			while ((*a)->index != target)
+				ra(a);
+		}
+		else
+		{
+			while ((*a)->index != target)
+				rra(a);
+		}
+		pb(a, b);
+		target++;
+	}
+	while (*b != NULL)
+		pa(a, b);
+}
